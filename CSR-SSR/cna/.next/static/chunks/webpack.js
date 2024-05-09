bacdc6d0f557e0ba1a6977a91f6bcb74cbbacc7e
qdrptd <qdrptd@qdrptdui-MacBookPro.local>
2024-05-09 13:13:32 +0900

    modified title of the slides

--- CSR-SSR/cna/.next/static/chunks/webpack.js
+++ CSR-SSR/cna/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7b97bde17c552747"; }
+/******/ 		__webpack_require__.h = function() { return "fe4304747e0ad01e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- project/pages/counters/counter1.tsx
+++ project/pages/counters/counter1.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 const counter1: NextPage = () =>{
     
     const [counts, setCounts] = useState<number[]>([0,]);
+    
     useEffect(() =>{
         setCounts([0]);
     },[])
